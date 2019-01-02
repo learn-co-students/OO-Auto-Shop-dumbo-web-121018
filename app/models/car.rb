82fa7d732eecb45ae02dd797248fe0520067f77c
github username <github email address>
2019-01-02 02:08:32 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,28 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :car_owner, :mechanic
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @car_owner = car_owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classification
+    self.all.map { |car| car.classification }
+  end
+
+  def self.mechanics(classify)
+    self.all.select { |car| car.classification == classify }.map { |car| car.mechanic }
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+kah = CarOwner.new("Kah")
+sion = CarOwner.new("Sion")
+corki = CarOwner.new("Corki")
+
+brigitte = Mechanic.new("Brigitte", "Exotic")
+paige = Mechanic.new("Paige", "Clunker")
+ziggs = Mechanic.new("Ziggs", "Antique")
+
+car1 = Car.new("car", "new", "Exotic", kah, brigitte)
+car2 = Car.new("car", "new", "Clunker", kah, paige)
+car3 = Car.new("car", "old", "Antique", sion, ziggs)
+car4 = Car.new("UFO", "old", "Antique", corki, ziggs)
+
+
 binding.pry
